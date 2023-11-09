ee79cf7418616c49a04c05185d544b0faba99d84
james1821 <jamesespinosamark@.com>
2023-11-09 10:42:15 +0800

    Added Ticket Components

---
+++ app/{components}/ProgressDisplay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProgressDisplay() {
+  return (
+    <div className="bg-gray-200  w-full rounded-md h-3">
+      <div className="bg-green-600 h-3 w-10"></div>
+    </div>
+  );
+}
+
+export default ProgressDisplay;
---
+++ app/{components}/StatusDisplay.jsx
@@ -0,0 +1,9 @@
+function StatusDisplay() {
+  return (
+    <div className="inline-block w-fit rounded-full px-2 py-1 text-xs font-semibold text-white-700 bg-green-600">
+      Done
+    </div>
+  );
+}
+
+export default StatusDisplay;
