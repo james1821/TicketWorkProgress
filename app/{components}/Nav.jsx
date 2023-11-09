858f9f1d5365587fb00da11a2f0baa172dd99082
james1821 <jamesespinosamark@.com>
2023-11-09 10:12:53 +0800

    Created Nav with static email (for now)

---
+++ app/{components}/Nav.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+function Nav() {
+  return (
+    <div className="flex justify-between items-center bg-black p-4 ">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <p>Home</p>
+        </Link>
+        <Link href="/TicketPage/new">
+          <p>Tickets</p>
+        </Link>
+      </div>
+      <div className="">
+        <p className="text-white">jamesespinosa@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
