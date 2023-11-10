8bb32d23abd391b8d6343212b2c09d5470cbd809
james1821 <jamesespinosamark@.com>
2023-11-10 08:45:54 +0800

    updated code for deployment

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -2,7 +2,7 @@ import EditTicketForm from "@/app/(components)/EditTicketForm";
 
 const getTicketById = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    const res = await fetch(`/api/Tickets/${id}`, {
       cache: "no-store",
     });
 
@@ -17,6 +17,7 @@ const getTicketById = async (id) => {
 };
 
 let updateTicketData = {};
+
 const TicketPage = async ({ params }) => {
   const EDITMODE = params.id === "new" ? false : true;
 
